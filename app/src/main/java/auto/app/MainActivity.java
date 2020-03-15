9529d9d97dc142524cef2e34f5c1fd0d73c3feea
malyshkin.yevgeniy@inbox.ru <iRUpA(pjOu22>
2020-03-15 04:54:44 -0600

    меню; обработка нажатий элементов меню;

--- app/src/main/java/auto/app/MainActivity.java
+++ app/src/main/java/auto/app/MainActivity.java
@@ -1,14 +1,44 @@
 package auto.app;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Boolean isFavFilterSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        isFavFilterSelected = false;
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addPost:
+                return true;
+            case R.id.fav:
+                isFavFilterSelected = !isFavFilterSelected;
+                if (isFavFilterSelected)
+                    item.setIcon(R.drawable.ic_favorite_white);
+                else
+                    item.setIcon(R.drawable.ic_favorite_border);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
