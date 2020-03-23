e9f46c2b6fd98bdc8ffd565f0b82da0f64e426db
malyshkin.yevgeniy@inbox.ru <iRUpA(pjOu22>
2020-03-23 16:27:27 -0600

    валидация полей; загрузка изображений на сервер; добавление объявлений в БД

--- app/src/main/java/auto/app/AddAdvertisementDialog.java
+++ app/src/main/java/auto/app/AddAdvertisementDialog.java
@@ -2,20 +2,39 @@ package auto.app;
 
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
+
+import auto.app.model.Advertisment;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -25,7 +44,13 @@ public class AddAdvertisementDialog extends BottomSheetDialogFragment {
     private Uri imageUri;
     private View view;
     private ImageView carImageView;
-
+    private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseRef;
+    private TextInputLayout price;
+    private TextInputLayout description;
+    private TextInputLayout title;
+    private ProgressDialog pd;
+    private StorageTask mTask;
 
     @NonNull
     @Override
@@ -34,11 +59,39 @@ public class AddAdvertisementDialog extends BottomSheetDialogFragment {
         view = View.inflate(getContext(), R.layout.add_advertisement_dialog, null);
         dialog.setContentView(view);
         mBehavior = BottomSheetBehavior.from((View) view.getParent());
+        mStorageRef = FirebaseStorage.getInstance().getReference("cars_pictures");
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("cars_advertisement");
+
         carImageView = view.findViewById(R.id.image_view_car);
         carImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openImage();
+            }
+        });
+        price = view.findViewById(R.id.textInputPrice);
+        title = view.findViewById(R.id.textInputName);
+        description = view.findViewById(R.id.textInputDescription);
+        Button save_button = view.findViewById(R.id.save_button);
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!validatePrice() | !validateDescription() | !validateTitle())
+                    return;
+                if (imageUri == null) {
+                    Toast.makeText(view.getContext(), "Необходимо выбрать фотографию", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (mTask == null || !mTask.isInProgress()) {
+                    Bundle bundle = getArguments();
+                    pd = new ProgressDialog(dialog.getContext());
+                    pd.setMessage("Пожалуйста, подождите . . .");
+//                    pd.setCancelable(false);
+//                    pd.show();
+                    uploadData();
+
+                }
             }
         });
         return dialog;
@@ -65,6 +118,78 @@ public class AddAdvertisementDialog extends BottomSheetDialogFragment {
             imageUri = data.getData();
             Picasso.get().load(imageUri).centerCrop().fit().into(carImageView);
         }
+    }
+
+    private boolean validateTitle() {
+        String titleS = Objects.requireNonNull(title.getEditText()).getText().toString().trim();
+
+        if (titleS.isEmpty()) {
+            title.setError("Поле не может быть пустым");
+            return false;
+        } else {
+            title.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validatePrice() {
+        String priceS = Objects.requireNonNull(price.getEditText()).getText().toString().trim();
+
+        if (priceS.isEmpty()) {
+            price.setError("Поле не может быть пустым");
+            return false;
+        } else {
+            price.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateDescription() {
+        String decriptionS = Objects.requireNonNull(description.getEditText()).getText().toString().trim();
+
+        if (decriptionS.isEmpty()) {
+            description.setError("Поле не может быть пустым");
+            return false;
+        } else {
+            description.setError(null);
+            return true;
+        }
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver cr = view.getContext().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cr.getType(uri));
+    }
+
+    private void uploadData() {
+        StorageReference fileReference = mStorageRef.child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+        mTask = fileReference.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        Task<Uri> urlTask = taskSnapshot.getStorage().getDownloadUrl();
+                        while (!urlTask.isSuccessful()) {
+                        }
+                        String downloadUrl = Objects.requireNonNull(urlTask.getResult()).toString();
+                        Advertisment advertisment = new Advertisment(Objects.requireNonNull(title.getEditText()).getText().toString(), Objects.requireNonNull(price.getEditText()).getText().toString(), Objects.requireNonNull(description.getEditText()).getText().toString(), downloadUrl);
+                        String advId = mDatabaseRef.push().getKey();
+                        if (advId != null) {
+                            mDatabaseRef.child(advId).setValue(advertisment);
+                            Toast.makeText(view.getContext(), "Объявление добавлено", Toast.LENGTH_SHORT).show();
+                            dismiss();
+                        } else
+                            Toast.makeText(view.getContext(), "Null Adv ID", Toast.LENGTH_SHORT).show();
+                        pd.dismiss();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(view.getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
 
---
+++ app/src/main/java/auto/app/model/Advertisment.java
@@ -0,0 +1,73 @@
+package auto.app.model;
+
+import com.google.firebase.database.Exclude;
+
+public class Advertisment {
+    private String mTitle;
+    private String mPrice;
+    private String mDescription;
+    private String mImageUrl;
+    private Boolean isFav;
+    private String key;
+
+    public Advertisment() {
+    }
+
+    public Advertisment(String mTitle, String mPrice, String mDescription, String mImageUrl) {
+        this.mTitle = mTitle;
+        this.mPrice = mPrice;
+        this.mDescription = mDescription;
+        this.mImageUrl = mImageUrl;
+        this.isFav = false;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmPrice() {
+        return mPrice;
+    }
+
+    public void setmPrice(String mPrice) {
+        this.mPrice = mPrice;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public void setmDescription(String mDescription) {
+        this.mDescription = mDescription;
+    }
+
+    public String getmImageUrl() {
+        return mImageUrl;
+    }
+
+    public void setmImageUrl(String mImageUrl) {
+        this.mImageUrl = mImageUrl;
+    }
+
+    public Boolean getFav() {
+        return isFav;
+    }
+
+    public void setFav(Boolean fav) {
+        isFav = fav;
+    }
+
+    @Exclude
+    public String getKey() {
+        return key;
+    }
+
+    @Exclude
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
